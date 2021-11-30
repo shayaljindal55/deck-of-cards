5e50858845ee7f987d6895a82bb6672ca33d1c42
Jindal, Shayal <shayal.jindal@accenture.com>
2021-11-30 14:47:07 +0000

    'Aces' are high in sort order

--- src/shared/Constants.js
+++ src/shared/Constants.js
@@ -1,6 +1,6 @@
 
 export const suits = ["♣︎", "♠︎", "♥︎", "♦︎"]; // defaulted as required sort order
-export const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+export const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
 export const redSuits = ["♥︎", "♦︎"]
 export const noDrawnCardSort = "There are no drawn cards to sort. Please draw cards from the deck."
-export const noCardsInDeck= "No cards in the deck."
+export const noCardsInDeck = "No cards in the deck."
